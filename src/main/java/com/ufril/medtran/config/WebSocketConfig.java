6983e91101717f7e8e4c42471a9fc95b65e3b22a
shadek <shadekurr.imf@gmail.com>
2024-11-03 19:03:44 +0600

    register gps web socket

--- src/main/java/com/ufril/medtran/config/WebSocketConfig.java
+++ src/main/java/com/ufril/medtran/config/WebSocketConfig.java
@@ -2,25 +2,24 @@ package com.ufril.medtran.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
 @Configuration
 @EnableWebSocketMessageBroker
-@Controller
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
-	@Override
-	public void configureMessageBroker(MessageBrokerRegistry config) {
-		config.enableSimpleBroker("/topic","/user","/queue");
-		config.setApplicationDestinationPrefixes("/app");
-	}
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic", "/user", "/queue");
+        config.setApplicationDestinationPrefixes("/app");
+    }
 
-	@Override
-	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/newMessage").setAllowedOrigins("*").withSockJS();
-	}
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/newMessage", "gps")
+                .setAllowedOrigins("*")
+                .withSockJS();
+    }
 
 }
